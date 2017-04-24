48dcb97e6fc662fc8e435f2689fbb749a4991fc2
ChallengerCY <git@790597004@qq.com>
2017-04-24 16:32:00 +0800

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.  On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	new file:   hellojQuery/index.html
    	new file:   hellojQuery/jquery-3.2.0.js
    	new file:   hellojQuery/testjQuery.js

---
+++ hellojQuery/testjQuery.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Administrator on 2017/4/6 0006.
+ */
+
+$(document).ready(function(){ //.ready方法在文件加载完执行
+    /*alert("文件加载成功");*/
+    $("p").click(function(){ //jQery的语法格式 $(元素).方法() .click是点击事件
+        $(this).hide(); //.hide()是隐藏
+    })
+})
+
+
